08ae5a6064c8b8b91c5d1a5e6961810be9bb044d
Viczmándi Gergely <viczmandi@gmail.com>
2016-05-17 22:08:40 +0200

    subpages renamed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function randomImage(){
+  for (var i = 1; i <= 6; i++) {
+    var rnd_numb = Math.floor((Math.random()*1084)+1);
+    var imgUrl = "https://unsplash.it/360?image=";
+    var randImg = imgUrl.concat(rnd_numb);
+    document.getElementById("randomImg" + i).setAttribute("src", randImg);
+  }
+}
